64b943dfdaf66c90437eb54ac5253949c8a4ef19
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-25 15:04:58 +0200

    Ajout de la classe Car

---
+++ Projets/Objective-C/Objective-C/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Objective-C
+//
+//  Created by Ludovic Ollagnier on 25/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ Projets/Objective-C/Objective-C/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Objective-C
+//
+//  Created by Ludovic Ollagnier on 25/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
