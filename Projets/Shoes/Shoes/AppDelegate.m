0bb4601e8085e356f5779a2d7f58790ea43474a9
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-26 12:25:50 +0200

    Demo

--- Projets/Shoes/Shoes/AppDelegate.m
+++ Projets/Shoes/Shoes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Shoes.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    Shoes *s = [[Shoes alloc] initWithBrand:@"Nike" color:@"Rose poudré" size:38 andType:ShoeTypeNight];
+
+    NSLog(@"%@", s);
+
     return YES;
 }
 
