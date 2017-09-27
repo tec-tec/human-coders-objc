f0e36eada52892624e8b111c63ea6b83959898a9
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-27 15:32:12 +0200

    Ajout de catégorie

---
+++ Projets/Shoes/Shoes/UIColor+MoreColors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+MoreColors.h
+//  Shoes
+//
+//  Created by Ludovic Ollagnier on 27/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (MoreColors)
+
+@end
---
+++ Projets/Shoes/Shoes/UIColor+MoreColors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+MoreColors.m
+//  Shoes
+//
+//  Created by Ludovic Ollagnier on 27/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import "UIColor+MoreColors.h"
+
+@implementation UIColor (MoreColors)
+
+@end
