34353472868424aaee91f0f2c73b5c8672ece7cc
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-27 16:13:17 +0200

    Ajout méthodes datasource

--- Projets/Shoes/Shoes/ShoesListViewController.m
+++ Projets/Shoes/Shoes/ShoesListViewController.m
@@ -24,6 +24,14 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
 /*
 #pragma mark - Navigation
 
