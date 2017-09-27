c9ae206a0fbaac8912a6f0c6d4e9d03871d739cc
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-27 16:01:25 +0200

    Ajout nouvelle classe pour la liste

---
+++ Projets/Shoes/Shoes/ShoesListViewController.m
@@ -0,0 +1,37 @@
+//
+//  ShoesListViewController.m
+//  Shoes
+//
+//  Created by Ludovic Ollagnier on 27/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import "ShoesListViewController.h"
+
+@interface ShoesListViewController ()
+
+@end
+
+@implementation ShoesListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
