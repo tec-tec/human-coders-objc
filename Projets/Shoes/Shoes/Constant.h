68767bee23b978a218f28ad90529a00c84e85ec0
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-09-29 09:49:47 +0200

    Ajout de constantes

---
+++ Projets/Shoes/Shoes/Constant.h
@@ -0,0 +1,12 @@
+//
+//  Constant.h
+//  Shoes
+//
+//  Created by Ludovic Ollagnier on 29/09/2017.
+//  Copyright © 2017 Ludovic Ollagnier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static NSString * const UserDefaultsKeyFirstName = @"UserDefaultsKeyFirstName";
+static NSString * const NotificationNameModelUpdated = @"NotificationNameModelUpdated";
